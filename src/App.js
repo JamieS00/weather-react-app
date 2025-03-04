b86978b94ca1741216d592c79eda61e79ca8f918
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2025-03-04 14:50:28 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ function App() {
   return (
     <div className="App">
       <h1>Weather Application</h1>
+      <footer>
+        <p>
+          This project was coded by Jamie Singleton and is{" "}
+          <a
+            href="https://github.com/JamieS00/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weatherr-reactt.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
