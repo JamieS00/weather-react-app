24915edd4e8ed32b4dcc7121ad827c535026dbc4
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2025-03-06 16:03:41 -0500

    css for weather & app component

--- src/Weather.js
+++ src/Weather.js
@@ -10,23 +10,29 @@ const Weather = () => {
             type="search"
             placeholder="Enter a city..."
             className="form-control"
+            autoFocus="on"
           />
-          <input type="submit" value="Search" className="btn btn-primary" />
+          <input
+            type="submit"
+            value="Search"
+            className="btn btn-primary w-100"
+          />
         </div>
       </form>
       <hr />
-      <h1>New York</h1>
+      <h1 className="mb-1">New York</h1>
       <ul>
         <li> Wednesday 07:00 </li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="cloudy"
           />
-          6°C
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
