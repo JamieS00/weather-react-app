63eb37ed971b5b4d125cc24b55ee07de95e5fa67
Jamie Singleton <43870204+JamieS00@users.noreply.github.com>
2025-03-06 15:06:31 -0500

    weather css, change class name

--- src/Weather.js
+++ src/Weather.js
@@ -4,33 +4,38 @@ import "./Weather.css";
 const Weather = () => {
   return (
     <div className="Weather">
-      <div className="weather-container">
-        <form action="handleSubmit" className="weather-search-bar mb-4">
-          <input type="search" placeholder="Enter a city..." />
-          <input type="submit" value="Search" />
-        </form>
-        <hr />
-        <h1>New York</h1>
-        <ul>
-          <li> Wednesday 07:00 </li>
-          <li>Mostly cloudy</li>
-        </ul>
+      <form>
         <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="cloudy"
-            />
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation: 15%</li>
-              <li>Humidity:72%</li>
-              <li>Wind:13 km/h%</li>
-            </ul>
-          </div>
-          <hr />
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            className="form-control"
+          />
+          <input type="submit" value="Search" className="btn btn-primary" />
         </div>
+      </form>
+      <hr />
+      <h1>New York</h1>
+      <ul>
+        <li> Wednesday 07:00 </li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="cloudy"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity:72%</li>
+            <li>Wind:13 km/h%</li>
+          </ul>
+        </div>
+        <hr />
       </div>
     </div>
   );
